5440304a4da2172fbfd094159229ae730b599f9f
Melkamu8 <melkamudi@yahoo.com>
2020-01-09 17:59:23 -0500

    Lesson_302

---
+++ src/main/java/com/example/demo/Job.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String title;
+
+    @NotNull
+    @Size(min=3)
+    private String employer;
+
+    @NotNull
+    @Size(min=10)
+    private String description;
+
+    public Job() {
+    }
+}
